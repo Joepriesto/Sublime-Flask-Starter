15ba124cea0bbeb4a06b09d3587507b8d8259fa4
joepriestman <joe.priestman@googlemail.com>
2017-06-26 23:58:12 +0100

    Corrected MAC typos + PEP8 Formatting
    Auto formatted to PEP8 standard and fixed typo that stopped the functioning on MAC OS.

--- flask-starter.py
+++ flask-starter.py
@@ -1,8 +1,11 @@
-import sublime,sublime_plugin,os, platform
+import sublime
+import sublime_plugin
+import os
+import platform
 
-IS_MAC = plaform.system() == "Darwin"
+MAC_OS_X = platform.system() == "Darwin"
 IS_LINUX = "linux" in platform.system()
-FLASK_CODE='''from flask import Flask
+FLASK_CODE = '''from flask import Flask
 
 app = Flask(__name__)
 
@@ -17,50 +20,57 @@ if __name__ == '__main__':
 
 
 class FlaskStarterBase(object):
-	@staticmethod
-	def createFolder(name,paths):
-		if not len(paths):
-			raise ValueError("No path data available from SideBar")
-		else:
-			path=paths[0]+'\\'+ name 
-			if MAC_OS_X or IS_LINUX:
-				path=paths[0]+'/'+ name 
-			if not os.path.exists(path):
-				os.makedirs(path)
-			return path
-	@staticmethod
-	def createSubFiles(name,path):
-		dirs=[path+'\\static',path+'\\templates']
-		if MAC_OS_X or IS_LINUX:
-			dirs=[path+'/static',path+'/templates']
-		for dir in dirs:
-			if not os.path.exists(dir):
-				os.makedirs(dir)
-		appPath=path+'\\'+name+'.py'
-		if MAC_OS_X or IS_LINUX:
-			appPath=path+'/'+name+'.py'
-		with open(appPath,'w') as f:
-			f.write(FLASK_CODE)
+    @staticmethod
+    def createFolder(name, paths):
+        if not len(paths):
+            raise ValueError("No path data available from SideBar")
+        else:
+            path = paths[0] + '\\' + name
+            if MAC_OS_X or IS_LINUX:
+                path = paths[0] + '/' + name
+            if not os.path.exists(path):
+                os.makedirs(path)
+            return path
+
+    @staticmethod
+    def createSubFiles(name, path):
+        dirs = [path + '\\static', path + '\\templates']
+        if MAC_OS_X or IS_LINUX:
+            dirs = [path + '/static', path + '/templates']
+        for dir in dirs:
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+        appPath = path + '\\' + name + '.py'
+        if MAC_OS_X or IS_LINUX:
+            appPath = path + '/' + name + '.py'
+        with open(appPath, 'w') as f:
+            f.write(FLASK_CODE)
+
 
 class RelativeflaskCommand(sublime_plugin.TextCommand):
-	def run(self,edit):
-		f=self.view.file_name()
-		self.path=[self.getFolderName(f)]
-		sublime.active_window().show_input_panel("Please enter Project Name:", '', lambda s: self.rest(s), None, None)
-	def rest(self,name):
-		folderName = FlaskStarterBase.createFolder(name,self.path)
-		FlaskStarterBase.createSubFiles(name,folderName)
-	def getFolderName(self,file):
-		if MAC_OS_X or IS_LINUX:
-			return file.replace(file.split('/')[-1],'')
-		return file.replace(file.split('\\')[-1],'')
+    def run(self, edit):
+        f = self.view.file_name()
+        self.path = [self.getFolderName(f)]
+        sublime.active_window().show_input_panel(
+            "Please enter Project Name:",
+            '', lambda s: self.rest(s), None, None)
+
+    def rest(self, name):
+        folderName = FlaskStarterBase.createFolder(name, self.path)
+        FlaskStarterBase.createSubFiles(name, folderName)
+
+    def getFolderName(self, file):
+        if MAC_OS_X or IS_LINUX:
+            return file.replace(file.split('/')[-1], '')
+        return file.replace(file.split('\\')[-1], '')
+
 
 class NewflaskCommand(sublime_plugin.WindowCommand):
-	def run(self, paths=[]):
-		self.path=paths
-		self.window.show_input_panel("Please enter Project Name:", '', lambda s: self.doRest(s), None, None)
+    def run(self, paths=[]):
+        self.path = paths
+        self.window.show_input_panel(
+            "Please enter Project Name:", '', lambda s: self.doRest(s), None, None)
 
-	def doRest(self,name):
-		folderName = FlaskStarterBase.createFolder(name,self.path)
-		FlaskStarterBase.createSubFiles(name,folderName)
-
+    def doRest(self, name):
+        folderName = FlaskStarterBase.createFolder(name, self.path)
+        FlaskStarterBase.createSubFiles(name, folderName)
