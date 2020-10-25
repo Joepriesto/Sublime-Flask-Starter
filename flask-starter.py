9b7bcac248c5ec52a14bf520135e90aeb0220bdc
Joe Priestman <joe.priestman@googlemail.com>
2020-10-25 01:59:40 +0100

    Application factory (#5)
    * Added commands and code
    
    * Updated camelCase to lowercase in command names

--- flask-starter.py
+++ flask-starter.py
@@ -15,6 +15,41 @@ def hello_world():
 if __name__ == '__main__':
     app.run()'''
 
+FLASK_FACTORY_CODE = '''
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
+'''
+
 
 class FlaskStarterBase(object):
     @staticmethod
@@ -32,14 +67,14 @@ class FlaskStarterBase(object):
 
 
     @staticmethod
-    def createSubFiles(name, path):
+    def createSubFiles(name, path, type="basic"):
         dirs = [os.path.join(path, 'static'), os.path.join(path, 'templates')]
         for d in dirs:
             if not os.path.exists(d):
                 os.makedirs(d)
         appPath = os.path.join(path, (name + '.py'))
         with open(appPath, 'w') as f:
-            f.write(FLASK_CODE)
+            f.write(FLASK_CODE) if type == "basic" else f.write(FLASK_FACTORY_CODE) if type == "factory" else f.write("App type not found.")
 
 
 class RelativeflaskCommand(sublime_plugin.TextCommand):
@@ -58,6 +93,21 @@ class RelativeflaskCommand(sublime_plugin.TextCommand):
     def getFolderName(file):
         return os.path.dirname(file)
 
+class RelativeflaskfactoryCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        f = self.view.file_name()
+        self.path = [self.getFolderName(f)]
+        sublime.active_window().show_input_panel(
+            "Please enter Project Name:",
+            '', self.rest, None, None)
+
+    def rest(self, name):
+        folderName = FlaskStarterBase.createFolder(name, self.path)
+        FlaskStarterBase.createSubFiles(name, folderName, "factory")
+
+    @staticmethod
+    def getFolderName(file):
+        return os.path.dirname(file)
 
 class NewflaskCommand(sublime_plugin.WindowCommand):
     def run(self, paths=None):
@@ -68,3 +118,13 @@ class NewflaskCommand(sublime_plugin.WindowCommand):
     def doRest(self, name):
         folderName = FlaskStarterBase.createFolder(name, self.path)
         FlaskStarterBase.createSubFiles(name, folderName)
+
+class NewflaskfactoryCommand(sublime_plugin.WindowCommand):
+    def run(self, paths=None):
+        self.path = paths or []
+        self.window.show_input_panel(
+            "Please enter Project Name:", '', self.doRest, None, None)
+
+    def doRest(self, name):
+        folderName = FlaskStarterBase.createFolder(name, self.path)
+        FlaskStarterBase.createSubFiles(name, folderName, "factory")
